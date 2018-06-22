657aae4f23adaa943b64d4302a1af1435d2be212
Ben Moodie <swellben@gmail.com>
2018-06-22 15:12:28 -0600

    Initial design before relocated side menu

---
+++ Build/assets/js/offcanvas.js
@@ -0,0 +1,20 @@
+//$('.off-canvas-toggle').on('click', function(event) {
+//  event.preventDefault();
+//  $('body').toggleClass('off-canvas-active');
+//});
+
+//$(document).on('mouseup touchend', function(event) {
+//  var offCanvas = $('.off-canvas')
+//  if (!offCanvas.is(event.target) && offCanvas.has(event.target).length === 0) {
+//    $('body').removeClass('off-canvas-active')
+//  }
+//});
+
+
+
+$(document).ready(function() {
+  $("#sidebarCollapse").on("click", function() {
+    $("#sidebar").toggleClass("active");
+    $(this).toggleClass("active");
+  });
+});
