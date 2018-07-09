ea90c1b8586926308292cbcf63d61d811464c65a
Ben Moodie <swellben@gmail.com>
2018-07-08 23:09:02 -0600

    Added custom themed bootstrap elements and Customized one month date range picker

---
+++ assets/js/datepicker.js
@@ -0,0 +1,11 @@
+$(function(){
+
+	$('#calendar-range').dateRangePicker(
+	{
+		inline:true,
+		container: '#calendar-range-container',
+		alwaysOpen:true,
+		singleMonth: true
+	});
+
+});
---
+++ dates/demo.js
@@ -0,0 +1,17 @@
+$(function()
+{
+
+
+
+	$('#date-range12').dateRangePicker(
+	{
+		inline:true,
+		container: '#date-range12-container',
+		alwaysOpen:true,
+		singleMonth: true
+	});
+
+
+
+
+});
